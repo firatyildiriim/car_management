52d94872569e8008d548c1b268c99b74c9209aab
firatyildiriim <firat.yld16@gmail.com>
2024-03-29 12:52:04 +0300

    creating module

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Company Car Management',
+    'author': 'Fırat Yıldırım',
+    'sequence': -101,
+    'application': True,
+    'category': 'Car Fleet Organization',
+    'version': '1.0.0'
+}
